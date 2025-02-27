e03829f393ed30e2df56e50f0bd3cb274c260775
Chop-Chopa <136185471+Chop-Chopa@users.noreply.github.com>
2025-02-27 23:14:16 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,70 @@
+from faker import Faker
+import file_operations
+import random
+import os
+
+
+OUTPUT_DIR = r"C:\school\dom"
+os.makedirs(OUTPUT_DIR, exist_ok=True)
+FAKE = Faker("ru_RU")
+SKILLS = ["Стремительный прыжок", "Электрический выстрел", "Ледяной удар", "Стремительный удар", "Кислотный взгляд", "Тайный побег", "Ледяной выстрел", "Огненный заряд"]
+NEW_WORDS = {
+    'а': 'а͠', 'б': 'б̋', 'в': 'в͒͠',
+    'г': 'г͒͠', 'д': 'д̋', 'е': 'е͠',
+    'ё': 'ё͒͠', 'ж': 'ж͒', 'з': 'з̋̋͠',
+    'и': 'и', 'й': 'й͒͠', 'к': 'к̋̋',
+    'л': 'л̋͠', 'м': 'м͒͠', 'н': 'н͒',
+    'о': 'о̋', 'п': 'п̋͠', 'р': 'р̋͠',
+    'с': 'с͒', 'т': 'т͒', 'у': 'у͒͠',
+    'ф': 'ф̋̋͠', 'х': 'х͒͠', 'ц': 'ц̋',
+    'ч': 'ч̋͠', 'ш': 'ш͒͠', 'щ': 'щ̋',
+    'ъ': 'ъ̋͠', 'ы': 'ы̋͠', 'ь': 'ь̋',
+    'э': 'э͒͠͠', 'ю': 'ю̋͠', 'я': 'я̋',
+    'А': 'А͠', 'Б': 'Б̋', 'В': 'В͒͠',
+    'Г': 'Г͒͠', 'Д': 'Д̋', 'Е': 'Е',
+    'Ё': 'Ё͒͠', 'Ж': 'Ж͒', 'З': 'З̋̋͠',
+    'И': 'И', 'Й': 'Й͒͠', 'К': 'К̋̋',
+    'Л': 'Л̋͠', 'М': 'М͒͠', 'Н': 'Н͒',
+    'О': 'О̋', 'П': 'П̋͠', 'Р': 'Р̋͠',
+    'С': 'С͒', 'Т': 'Т͒', 'У': 'У͒͠',
+    'Ф': 'Ф̋̋͠', 'Х': 'Х͒͠', 'Ц': 'Ц̋',
+    'Ч': 'Ч̋͠', 'Ш': 'Ш͒͠', 'Щ': 'Щ̋',
+    'Ъ': 'Ъ̋͠', 'Ы': 'Ы̋͠', 'Ь': 'Ь̋',
+    'Э': 'Э͒͠͠', 'Ю': 'Ю̋͠', 'Я': 'Я̋',
+    ' ': ' '
+}
+
+
+def generate_character():
+    luck_skills = random.sample(SKILLS, 3)
+    runic_skills = []
+    for skill in luck_skills:
+        text = ""
+        for char in skill:
+            text += NEW_WORDS.get(char)
+        runic_skills.append(text)
+    return {
+        "first_name": FAKE.first_name(),
+        "last_name": FAKE.last_name(),
+        "job": FAKE.job(),
+        "town": FAKE.city(),
+        "strength": random.randint(3, 18),
+        "agility": random.randint(3, 18),
+        "endurance": random.randint(3, 18),
+        "intelligence": random.randint(3, 18),
+        "luck": random.randint(3, 18),
+        "skill_1": runic_skills[0],
+        "skill_2": runic_skills[1],
+        "skill_3": runic_skills[2],
+    }
+
+
+def main():
+    for i in range(1, 11):
+        context = generate_character()
+        file_path = os.path.join(OUTPUT_DIR, f"result_{i}.svg")
+        file_operations.render_template("charsheet.svg", file_path, context)
+
+
+if __name__ == '__main__':
+    main()
